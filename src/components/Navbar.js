eec8cc1c5e0655c192b7481a93bad5a79ad25910
Julija S <julija.sesok@gmail.com>
2023-11-06 17:05:15 +0100

    First commit - create a Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { ReactComponent as CartIcon } from "../images/icon-cart.svg";
+import Avatar from "../images/image-avatar.png";
+
+function Navbar() {
+  return (
+    <ul>
+      <li><h1>sneakers</h1></li>
+      <li><a href="#">Collections</a></li>
+      <li><a href="#">Men</a></li>
+      <li><a href="#">Women</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#"><CartIcon /></a></li>
+      <li><a href="#"><img src={Avatar} alt="Avatar"/></a></li>
+    </ul>
+  )
+}
+
+export default Navbar;
