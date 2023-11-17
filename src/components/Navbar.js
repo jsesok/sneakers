79bd7c6a40364a70f92bb96e2db0f2f259c16b29
Julija S <julija.sesok@gmail.com>
2023-11-17 10:56:39 +0100

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,20 @@ import Avatar from "../images/image-avatar.png";
 
 function Navbar() {
   return (
-    <ul className="navbar">
-      <li><h1>sneakers</h1></li>
-      <li><a href="#">Collections</a></li>
-      <li><a href="#">Men</a></li>
-      <li><a href="#">Women</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#"><CartIcon /></a></li>
-      <li><a href="#"><img src={Avatar} alt="Avatar"/></a></li>
-    </ul>
+    <nav className="navbar">
+      <ul className="left-items">
+        <li><h1>sneakers</h1></li>
+        <li><a href="#">Collections</a></li>
+        <li><a href="#">Men</a></li>
+        <li><a href="#">Women</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul className="right-items">
+        <li><a href="#"><CartIcon /></a></li>
+        <li><a href="#"><img src={Avatar} alt="Avatar"/></a></li>
+      </ul>
+    </nav>
   )
 }
 
